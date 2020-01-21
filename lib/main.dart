6f8d1e5c58da90e1f0ac6c6f854ab4257b7fdfba
oussamabonnor1 <oussamabonnor@gmail.com>
2020-01-21 19:47:34 +0100

    adding transaction info card widget

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,9 @@ class _HomePageState extends State<HomePage> {
   static Color darkAccentColor = Color(0xFF951556);
   static Color lightAccentColor = Color(0xFFE9B4D2);
   static Color darkUiColor = Color(0xFF4a3943);
-  static Color darkTextColor = Color(0xFFE3E3E3);
+  static Color darkTextColor = Color(0xFF565656);
   static Color lightTextColor = Color(0xFFFEFEFE);
+  static Color lightTransparentTextColor = Color(0xFFDCDCDC);
 
   @override
   Widget build(BuildContext context) {
@@ -49,24 +50,20 @@ class _HomePageState extends State<HomePage> {
                         style: TextStyle(
                             fontSize: 14,
                             fontStyle: FontStyle.italic,
-                            color: darkTextColor)),
-                    SizedBox(height: 10),
+                            color: lightTransparentTextColor)),
                     Text(
                       "0.00 Da",
                       style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: darkTextColor),
+                          fontSize: 20, color: lightTransparentTextColor),
                     ),
                     SizedBox(
-                      height: 20,
+                      height: 15,
                     ),
                     Text("Gain du jour:",
                         style: TextStyle(
                             fontSize: 17,
-                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.bold,
                             color: lightTextColor)),
-                    SizedBox(height: 10),
                     Text(
                       "0.00 Da",
                       style: TextStyle(
@@ -106,16 +103,18 @@ class _HomePageState extends State<HomePage> {
                     onPressed: () {})
               ],
             ),
+            SizedBox(
+              height: 10,
+            ),
             Expanded(
                 child: ListView.separated(
-                    separatorBuilder: (context, index) => Divider(
-                          height: 3,
-                          color: Colors.transparent,
-                        ),
+                    separatorBuilder: (context, index) =>
+                        Divider(height: 3, color: Colors.transparent),
                     itemCount: 5,
-                    padding: EdgeInsets.fromLTRB(15, 5, 15, 0),
+                    padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
                     itemBuilder: (context, index) {
-                      return salesInfoCard(index);
+                      return salesInfoCard(index % 2 == 0, index,
+                          "Explication & infos & application", 16.3, 20);
                     }))
           ],
         ),
@@ -123,11 +122,62 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget salesInfoCard(int index) {
+  Widget salesInfoCard(
+      bool type, int index, String title, double weight, double price) {
     return Card(
-      color: lightAccentColor,
-      child: Text(
-        "$index",
+      color: lightTextColor,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 12, 16, 12),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            CircleAvatar(
+                backgroundColor: type ? lightAccentColor : darkUiColor,
+                child: Icon(
+                  type ? Icons.add : Icons.remove,
+                  color: lightTextColor,
+                )),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16, right: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(title,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: darkTextColor)),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text("$weight g",
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontStyle: FontStyle.italic,
+                            color: Color(0xFFA0A0A0))),
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Text(price.toString() + " Da",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[600])),
+                Icon(
+                  Icons.info_outline,
+                  color: Colors.grey[600],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
