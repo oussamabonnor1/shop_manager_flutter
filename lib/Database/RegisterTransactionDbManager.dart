23ffef063b64adf5c37d01f9d864167aa857db73
oussamabonnor1 <oussamabonnor@gmail.com>
2020-02-04 16:16:46 +0100

    adding Register Transaction DB manager

---
+++ lib/Database/RegisterTransactionDbManager.dart
@@ -0,0 +1,41 @@
+import 'package:shop_manager/Models/RegisterTransaction.dart';
+import 'package:sqflite/sqflite.dart';
+
+class RegisterTransactionDbManager {
+
+  Database db;
+  String tableName = "registerTransaction";
+  String messageColumn = "message";
+  String dateColumn = "date";
+  String valueColumn = "value";
+  String typeColumn = "type";
+
+  RegisterTransactionDbManager(this.db);
+
+  Future<RegisterTransaction> insert(RegisterTransaction registerTransaction) async {
+    print(registerTransaction.toMap().values.toList());
+    await db.insert(tableName, registerTransaction.toMap());
+    return registerTransaction;
+  }
+
+  Future<List<RegisterTransaction>> getAllSalesInfo() async {
+    List<Map> maps = await db.query(tableName);
+
+    if (maps.length > 0) {
+      maps = maps.reversed.toList();
+      List<RegisterTransaction> registerTransactionsList = List();
+      maps.forEach((element) => registerTransactionsList.add(RegisterTransaction.fromMap(element)));
+      return registerTransactionsList;
+    }
+    return null;
+  }
+
+  Future<int> deleteRegisterTransaction(String message) async {
+    return await db.delete(tableName, where: '$messageColumn = ?', whereArgs: [message]);
+  }
+
+  Future<int> update(RegisterTransaction registerTransaction) async {
+    return await db.update(tableName, registerTransaction.toMap(),
+        where: '$messageColumn = ?', whereArgs: [registerTransaction.message]);
+  }
+}
